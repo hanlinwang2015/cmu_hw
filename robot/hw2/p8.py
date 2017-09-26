681450721095bb5f76a3dc21cf87fc2aeb1df821
hanlinwang2015 <hanlinwang2015@u.northwestern.edu>
2017-09-26 00:35:33 -0500

    pro 8

--- robot/hw2/p8.py
+++ robot/hw2/p8.py
@@ -1,7 +1,18 @@
 #! /usr/bin/python
 
 import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
 
+x = np.linspace(-1.0, 1.0, 1000)
+y = np.linspace(-1.0, 1.0, 1000)
+X, Y = np.meshgrid(x,y)
+F = 2 * X**2 + 2 * Y**2 - 1.0
+F1 = X ** 2 + Y ** 2 + 2 * X * Y + X - Y
+plt.contour(X,Y,F,[0])
+
+plt.contour(X,Y,F1,[0])
+plt.show()
 def getiput():
 	x = sp.symbols('x')
 	y = sp.symbols('y')
