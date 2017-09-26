f1bd680f8ce8444b233a6e0967d181112735a99e
hanlinwang2015 <hanlinwang2015@u.northwestern.edu>
2017-09-26 11:34:46 -0500

    p9

--- robot/hw2/p9.py
+++ robot/hw2/p9.py
@@ -6,35 +6,55 @@ import numpy as np
 import math
 filename = "Image Data.txt"
 data = []
-for i in range(0, 50):
+for i in range(0, 98):
 	reader = csv.reader(open('/home/hanlin/cmu_hw/robot/hw2/paths.txt'),delimiter=" ")
 	col = list(zip(*zip(*reader)))[i] 
 	data.append(col)
-	print(col)
+#	print(i)
 temp = 1440 * [0.0]
-def F(n, xx, yy):
-	if n == 1: 
-		temp[n] = yy[0]
-	else:
-		num = (n - 1) * n / 2
-		for i in range(num + 1, num + n + 1):
-			if i == (num + 1):
-				temp[i] = yy[n - 1]
-#				print(yy[n - 1])
-			else:
-				temp[i] =  (temp[i - 1] - temp[i - n]) / (xx[n - 1] - xx[n - i + num])
-	
-def getP(xx, yy):
-	result = []
-	for i in range(1, len(xx) + 1):
-		F(i, xx, yy)
-		result.append(temp[(i + 1) * i / 2])
-#	print(temp[1:100])
-	return result
-	
-def getvalue(xx, yy, ff, tar, n):
-	if n == len(xx) - 1:
-		return ff[n]
-	else:
-		return getvalue(xx, yy, ff, tar, n + 1) * (tar - xx[n]) + ff[n]
+print(float(data[0][0]) - 1)
+def convex(x1, y1, x2, y2, x3, y3, x, y):
+	vx0 = x1
+	vy0 = y1
+	vx1 = x2 - x1
+	vy1 = y2 - y1
+	vx2 = x3 - x1
+	vy2 = y3 - y1
+	kk = vx1 * vy2 - vy1 * vx2
+	dist = ((x - x1)**2 + (y - y1)**2)**0.5 + ((x - x2)**2 + (y - y2)**2)**0.5 + ((x-x3)**2 + (y - y3)**2)**0.5
+#	print(kk)
+	if abs(kk) > 0.000000001:
 
+		a = (x *  vy2 - y * vx2 - vx0 * vy2 + vy0 * vx2) / kk
+		b = (x * vy1 - y * vx1 - vx0 * vy2 + vy0 * vx2) / kk
+		if a >= 0 and b >= 0 and a + b < 1:
+			return 1, a, b, dist
+		else:
+			return 0, a, b, dist
+	else:
+		if abs(x *  vy2 - y * vx2 - vx0 * vy2 + vy0 * vx2) < 0.00000001:
+			return 1, -1, -1, dist
+		else:
+			return 0, -1, -1, dist
+	#print(convex(0.0, 0.0, 1.0, 0.0, 1/2.0, 1/2.0, 3.0, 1.0))
+def weight(x, y):
+	mina = -1.0
+	for i in range(0, 49):
+		for j in range(i, 49):
+			for k in range(j,49):
+				if i != j and j != k:
+#					print(i, j, k)
+					det = convex(float(data[2 * i][0]), float(data[2 * i + 1][0]),float(data[2 * j][0]), float(data[2 * j + 1][0]), float(data[2 * k][0]), float(data[2 * k + 1][0]), x, y)
+		
+					if det[0]  == 1:
+						maxdist = 0
+						for m in range (1, 50):
+							if (abs(float(data[2 * i + 1][m]) - float(data[2 * j + 1][m])) > 3 and abs(float(data[2 * i][m])) > 3.5 and abs(float(data[2 * i][m])) < 6.5) or (abs(float(data[2 * k + 1][m]) - float(data[2 * j + 1][m])) > 3 and abs(float(data[2 * k][m])) > 3.5 and abs(float(data[2 * k][m])) < 6.5):
+								continue
+							else:	
+								if mina > det[3] or mina < 0:
+									mina = det[3]
+									cadidate = [i, j, k]
+									fdet = det
+	return cadidate, fdet
+print(weight(0.8, 1.8))
