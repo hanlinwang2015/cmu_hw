b8ab035cb313e2883e65f8b024f5b5abf42ea097
hanlinwang2015 <hanlinwang2015@u.northwestern.edu>
2017-09-26 14:27:38 -0500

    po5 fixed

--- robot/hw2/p5.py
+++ robot/hw2/p5.py
@@ -4,7 +4,7 @@ import math
 def newton(initial):
 	iterate = 0
 	xx = initial
-	while abs(xx - math.tan(xx)) > 0.000001 and iterate < 10000000:
+	while abs(math.cos(xx) * xx - math.sin(xx)) > 0.000001 and iterate < 10000000:
 		print(xx)
 		#while (math.cos(xx)) < 0.000001 and math.cos(xx) > 0:
 			#xx += 0.01
@@ -12,7 +12,7 @@ def newton(initial):
 	#	while math.cos(xx) > -0.0000001 and math.cos(xx) < 0:
 			#xx -= 0.01
 	#		continue
-		xx -= (1 / math.tan(xx) - xx / pow(math.tan(xx), 2))
+		xx = xx+ (math.cos(xx) * xx - math.sin(xx)) / (math.sin(xx) * xx)
 		iterate += 1
 	if xx - math.tan(xx) > 0.01:
 		print("bad initial guess, try again")
@@ -20,4 +20,4 @@ def newton(initial):
 		print("the solution:", xx, xx - math.tan(xx))
 	
 	
-newton(1.57 * 2)
+newton(5)
